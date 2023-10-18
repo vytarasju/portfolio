d29cddb53a496940e0f907d7e00ecffeddc007e3
vytarasju <juraskavytaras@gmail.com>
2023-10-19 01:33:12 +0800

    init nextjs

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  // changingFields = ["Front-end developemnt", "Back-end development"]
+  return (
+    <>
+      <h1>Hey, I'm Vytaras Juraska</h1>
+      <p> I am a passionate ciubmas in many various software development fields, such as:</p>
+      {/* <div text = {changingFields}></div> */}
+    </>
+  )
+}
